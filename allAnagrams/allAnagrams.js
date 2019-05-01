12c2da6d793dc925da00b1bbfc138a52bfa87d16
elimoreh <elimoreh@berkeley.edu>
2019-05-01 11:54:19 -0700

    implement all anagrams using sorting, passes example test

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -1,35 +1,18 @@
-/**
- * Given a single input string, write a function that produces all possible anagrams
- * of a string and outputs them as an array. At first, don't worry about
- * repeated strings.  What time complexity is your solution?
- *
- * Extra credit: Deduplicate your return array without using uniq().
- */
-
-/**
-  * example usage:
-  * var anagrams = allAnagrams('abc');
-  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
-
 var allAnagrams = function (string) {
   string = string.toLowerCase();
   let results = [];
-
+  var sorted = string.split('').sort().join('');
   let combinations = (combo = '') => {
     if (combo.length === string.length) {
-      for (var i = 0; i < string.length; i++) {
-        if (!combo.includes(string[i])) {
-          return;
+        if (sorted === combo.split('').sort().join('')) {
+          results.push(combo);
         }
-      };
-      results.push(combo);
       return;
     }
 
     for (var i = 0; i < string.length; i++) {
       combinations(combo + string[i]);
-    ;}
+    };
   };
 
   combinations();
