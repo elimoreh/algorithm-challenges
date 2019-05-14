762e78281b403294d7de57ba6f21d2f58ba9204a
elimoreh <elimoreh@berkeley.edu>
2019-05-14 09:17:47 -0700

    implement solution for breadth first tree filter-- passes all example test

--- treeBFSelect/treeBFSelect.js
+++ treeBFSelect/treeBFSelect.js
@@ -1,84 +1,85 @@
 /**
-  *
-  * Implement a `BFSelect` method on this Tree class.
-  *
-  * BFSelect accepts a filter function, calls that function on each of the nodes
-  * in Breadth First order, and returns a flat array of node values of the tree
-  * for which the filter returns true.
-  *
-  * Example:
-  *   var root1 = new Tree(1);
-  *   var branch2 = root1.addChild(2);
-  *   var branch3 = root1.addChild(3);
-  *   var leaf4 = branch2.addChild(4);
-  *   var leaf5 = branch2.addChild(5);
-  *   var leaf6 = branch3.addChild(6);
-  *   var leaf7 = branch3.addChild(7);
-  *   root1.BFSelect(function (value, depth) {
-  *     return value % 2;
-  *   })
-  *   // [1, 3, 5, 7]
-  *
-  *   root1.BFSelect(function (value, depth) {
-  *     return depth === 1;
-  *   })
-  *   // [2, 3]
-  *
-  */
+ *
+ * Implement a `BFSelect` method on this Tree class.
+ *
+ * BFSelect accepts a filter function, calls that function on each of the nodes
+ * in Breadth First order, and returns a flat array of node values of the tree
+ * for which the filter returns true.
+ *
+ * Example:
+ *   var root1 = new Tree(1);
+ *   var branch2 = root1.addChild(2);
+ *   var branch3 = root1.addChild(3);
+ *   var leaf4 = branch2.addChild(4);
+ *   var leaf5 = branch2.addChild(5);
+ *   var leaf6 = branch3.addChild(6);
+ *   var leaf7 = branch3.addChild(7);
+ *   root1.BFSelect(function (value, depth) {
+ *     return value % 2;
+ *   })
+ *   // [1, 3, 5, 7]
+ *
+ *   root1.BFSelect(function (value, depth) {
+ *     return depth === 1;
+ *   })
+ *   // [2, 3]
+ *
+ */
 
 /*
  * Basic tree that stores a value.
  */
 
-var Tree = function(value) {
+var Tree = function (value) {
   this.value = value;
   this.children = [];
 };
 
 
 
-Tree.prototype.BFSelect = function(filter) {
+Tree.prototype.BFSelect = function (filter) {
   // return an array of values for which the function filter(value, depth) returns true
   let results = [];
   let branchArray = [];
 
-   let iterateTree = function(tree, counter = -1){
+  let iterateTree = function (tree, counter = -1) {
     counter++;
 
-     if(!branchArray[counter]){
+    if (!branchArray[counter]) {
       branchArray[counter] = [tree.value]
     } else {
       branchArray[counter].push(tree.value);
     }
 
-     if(tree.children){
-      for(var i = 0; i < tree.children.length; i++){
+    if (tree.children) {
+      for (var i = 0; i < tree.children.length; i++) {
         iterateTree(tree.children[i], counter);
       }
     }
   }
 
   iterateTree(this);
+  
   branchArray.forEach((el, index) => {
-    for(var i = 0; i < el.length; i++){
-      if(filter(el[i],index)){
+    for (var i = 0; i < el.length; i++) {
+      if (filter(el[i], index)) {
         results.push(el[i]);
       }
     }
   });
 
-   return results;
+  return results;
 };
 
 /**
  * You shouldn't need to change anything below here, but feel free to look.
-  */
+ */
 
 /**
-  * add an immediate child
-  * (wrap values in Tree nodes if they're not already)
-  */
-Tree.prototype.addChild = function(child) {
+ * add an immediate child
+ * (wrap values in Tree nodes if they're not already)
+ */
+Tree.prototype.addChild = function (child) {
   if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
   }
@@ -93,10 +94,10 @@ Tree.prototype.addChild = function(child) {
 };
 
 /**
-  * check to see if the provided tree is already a child of this
-  * tree __or any of its sub trees__
-  */
-Tree.prototype.isDescendant = function(child) {
+ * check to see if the provided tree is already a child of this
+ * tree __or any of its sub trees__
+ */
+Tree.prototype.isDescendant = function (child) {
   if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
@@ -112,9 +113,9 @@ Tree.prototype.isDescendant = function(child) {
 };
 
 /**
-  * remove an immediate child
-  */
-Tree.prototype.removeChild = function(child) {
+ * remove an immediate child
+ */
+Tree.prototype.removeChild = function (child) {
   var index = this.children.indexOf(child);
   if (index !== -1) {
     // remove the child
