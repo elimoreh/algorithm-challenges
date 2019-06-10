9d7b44f8f4dc2457deec2b4baf75c21519d5b7bd
elimoreh <elimoreh@berkeley.edu>
2019-06-10 16:16:25 -0700

    import from LeetCode, adjust for base case

--- Pascal's_Triangle.js
+++ Pascal's_Triangle.js
@@ -1,22 +1,27 @@
+
+    
 /**
  * @param {number} numRows
  * @return {number[][]}
  */
- var generate = (numRows) => {
-    //Deals with 0 row edge Case
-    if(!numRows) {return [];}
-    let results = [[1]];
+var generate = (numRows) => {
+  //Deals with 0 row edge Case
+  if(!numRows) {return [];}
+  let results = [[1]];
 
-    let pascal = (array = [1]) => {
-        if(results.length >= numRows) { return; }
-        let sums = [];
+  let pascal = (array = [1]) => {
+      if(results.length >= numRows) { return; }
+      let sums = [];
+      for(let i = 0; i < array.length - 1; i++){
+        sums.push(array[i] + array[i + 1]);
+      }
 
-        //uses spread operator to insert the array containg the sums
-        let newArray = [1,...sums,1];
-        results.push(newArray);
-        pascal(newArray);
-   }
+      //uses spread operator to insert the array containg the sums
+      let newArray = [1,...sums,1];
+      results.push(newArray);
+      pascal(newArray);
+ }
 
-  pascal();
-  return results;
+pascal();
+return results;
 };
